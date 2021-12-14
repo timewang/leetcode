f13de1d7ba71049b3ebeccd456be554c04c74d5a
chandler <chandler@staritgp.com>
2021-12-14 09:43:42 +0800

    最长公共前缀

---
+++ src/main/java/org/snailgary/RomanToInteger.java
@@ -0,0 +1,40 @@
+package org.snailgary;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * <p>
+ *     https://leetcode-cn.com/problems/roman-to-integer/
+ * </p>
+ *
+ * @author chandler
+ * @since 2021/12/1
+ */
+public class RomanToInteger {
+
+    Map<Character, Integer> symbolValues = new HashMap<Character, Integer>() {{
+        put('I', 1);
+        put('V', 5);
+        put('X', 10);
+        put('L', 50);
+        put('C', 100);
+        put('D', 500);
+        put('M', 1000);
+    }};
+
+    public int romanToInt(String s) {
+        int ans = 0;
+        int n = s.length();
+        for (int i = 0; i < n; ++i) {
+            int value = symbolValues.get(s.charAt(i));
+            if (i < n - 1 && value < symbolValues.get(s.charAt(i + 1))) {
+                ans -= value;
+            } else {
+                ans += value;
+            }
+        }
+        return ans;
+    }
+
+}
